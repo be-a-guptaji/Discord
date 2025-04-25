a8ba3f9bcfd48fb21fd3e3600083144efdbb529b
Aryan Baadlas <154962185+be-a-guptaji@users.noreply.github.com>
2025-04-25 18:13:32 +0530

    Adds invite code handling and server membership logic
    Implements a new page for handling invite codes, allowing users to join servers based on their invite codes.
    
    Checks for user profiles and existing memberships before adding new members to servers.
    
    Ensures invite codes are unique in the database schema.
    
    Fixes #456

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,77 @@
+// app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+
+import getCurrentProfile from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface InviteCodePageProps {
+  params: Promise<{
+    inviteCode: string;
+  }>;
+}
+
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+  // Fetch the current profile
+  const profile = await getCurrentProfile();
+
+  // Check if the profile is null or undefined
+  if (!profile) {
+    redirect("/");
+  }
+
+  // Extract the inviteCode from the params
+  const { inviteCode } = await params;
+
+  // Check if the inviteCode is null or undefined
+  if (!inviteCode) {
+    redirect("/");
+  }
+
+  // Check if user already exists in the server
+  const existingInServer = await db.server.findFirst({
+    where: {
+      inviteCode,
+      members: {
+        some: {
+          profileID: profile.id,
+        },
+      },
+    },
+  });
+
+  // If the user already exists in the server, redirect to the server page
+  if (existingInServer) {
+    redirect(`/server/${existingInServer.id}`);
+  }
+
+  try {
+    // Check if the server with the given inviteCode exists
+    const server = await db.server.update({
+      where: {
+        inviteCode,
+      },
+      data: {
+        members: {
+          create: [
+            {
+              profileID: profile.id,
+            },
+          ],
+        },
+      },
+    });
+
+    // If the server with the given inviteCode exist, redirect to the home page
+    if (server) {
+      redirect(`/server/${server.id}`);
+    }
+  } catch (error) {
+    // Handle the case where the server with the given inviteCode does not exist
+    console.error("Error no server found:", error);
+    redirect("/"); // Redirect to home page if the server does not exist
+  }
+
+  return null; // Return null if no redirection occurs
+};
+
+export default InviteCodePage;
